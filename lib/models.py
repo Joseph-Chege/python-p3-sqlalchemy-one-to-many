d87d7cf0038f2d3e52642a1fadede9136416d9ef
Joseph-Chege <mjchege23@gmail.com>
2024-07-22 21:37:46 +0300

    Went through one-to-many relationships with SQLAlchemy

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,31 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+#Game model
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String(), nullable=False)
+    genre = Column(String(), nullable=False)
+    platform = Column(String(), nullable=False)
+    price = Column(Integer(), nullable=False)
+
+    reviews = relationship('Review', backref=backref('game'))
+
+    def __repr__(self):
+        return f'Game(title="{self.title}", genres="{self.genre}", platform="{self.platform}")'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer(), nullable=False)
+    comment = Column(String(), nullable=False)
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(score={self.score}, comment="{self.comment}", game_id="{self.game_id}")'
+
+    
